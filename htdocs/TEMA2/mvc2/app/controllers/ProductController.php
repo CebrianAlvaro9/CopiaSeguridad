630ed0b33c96dc03b369f28bd47f457b81623924
CebrianAlvaro9 <113922067+CebrianAlvaro9@users.noreply.github.com>
2022-11-10 17:22:01 +0100

    servidor

---
+++ htdocs/TEMA2/mvc2/app/controllers/ProductController.php
@@ -0,0 +1,18 @@
+<?php
+
+class ProductController{
+    function __construct()
+    {
+        echo "<br> Constructor clase PRODUCTCONTROLLER";
+
+    }//FIN CONSTRUCCTOR
+
+    function index(){
+        echo "<br> index clase PRODUCTCONTROLLER";
+    }// metodo home de Controller de mvc
+
+    function show(){
+        echo "<br> show clase PRODUCTCONTROLLER";
+
+    }// 
+}
